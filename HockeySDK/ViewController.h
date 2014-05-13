9225d98e2aa974e4df287a9d38f62d871e155682
lalitpaliwal <lalit.paliwal@yash.com>
2014-05-13 16:33:31 +0530

    Initial Commit

---
+++ HockeySDK/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HockeySDK
+//
+//  Created by Lalit Paliwal on 13/05/14.
+//  Copyright (c) 2014 YASH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HockeySDKTests/HockeySDKTests.m
@@ -0,0 +1,34 @@
+//
+//  HockeySDKTests.m
+//  HockeySDKTests
+//
+//  Created by Lalit Paliwal on 13/05/14.
+//  Copyright (c) 2014 YASH. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HockeySDKTests : XCTestCase
+
+@end
+
+@implementation HockeySDKTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
